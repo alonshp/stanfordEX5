97644547731c27763db6248af120ebfcd1ae0a24
Alon Shprung <alonshprung@Alons-MacBook-Pro.local>
2018-04-01 17:19:51 +0300

    can drag and drop images and fetch them with their URL

---
+++ Image Gallery/ImageGalleryCollectionViewCell.swift
@@ -0,0 +1,19 @@
+//
+//  ImageGalleryCollectionViewCell.swift
+//  Image Gallery
+//
+//  Created by Alon Shprung on 4/1/18.
+//  Copyright © 2018 Alon Shprung. All rights reserved.
+//
+
+import UIKit
+
+class ImageGalleryCollectionViewCell: UICollectionViewCell {
+    
+    @IBOutlet weak var imageView: UIImageView!
+    
+    @IBOutlet weak var cellHeight: NSLayoutConstraint!
+    
+    @IBOutlet weak var cellWidth: NSLayoutConstraint!
+    
+}
--- Image Gallery/ViewController.swift
+++ Image Gallery/ViewController.swift
@@ -8,18 +8,112 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UICollectionViewDelegate, UICollectionViewDataSource, UICollectionViewDropDelegate, UICollectionViewDragDelegate {
+    
+    
+    private var images = [UIImage]()
+    
+    var imageFetcher: ImageFetcher!
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+    @IBOutlet weak var imageGalleryView: UICollectionView! {
+        didSet {
+            imageGalleryView.dataSource = self
+            imageGalleryView.delegate = self
+            imageGalleryView.dragDelegate = self
+            imageGalleryView.dropDelegate = self
+        }
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return images.count
     }
-
-
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "imageCell", for: indexPath)
+        if let imageCell = cell as? ImageGalleryCollectionViewCell {
+            imageCell.imageView.image = images[indexPath.item]
+        }
+        return cell
+    }
+    
+    
+    func collectionView(_ collectionView: UICollectionView, performDropWith coordinator: UICollectionViewDropCoordinator) {
+        let destinationIndexPath = coordinator.destinationIndexPath ?? IndexPath(item: 0, section: 0)
+        for item in coordinator.items {
+            // item is already at the collection view
+            if let sourceIndexPath = item.sourceIndexPath {
+                if let image = item.dragItem.localObject as? UIImage {
+                    collectionView.performBatchUpdates({
+                        images.remove(at: sourceIndexPath.item)
+                        images.insert(image, at: destinationIndexPath.item)
+                        collectionView.deleteItems(at: [sourceIndexPath])
+                        collectionView.insertItems(at: [destinationIndexPath])
+                    })
+                    coordinator.drop(item.dragItem, toItemAt: destinationIndexPath)
+                }
+                // new item
+            } else {
+                let placeholderContext = coordinator.drop(
+                    item.dragItem,
+                    to: UICollectionViewDropPlaceholder(insertionIndexPath: destinationIndexPath, reuseIdentifier: "DropPlaceholderCell"))
+                
+                // load image
+                item.dragItem.itemProvider.loadObject(ofClass: UIImage.self) { (provider, error) in
+                    // TODO: aspect ratio
+                }
+                
+                // load image url
+                item.dragItem.itemProvider.loadObject(ofClass: NSURL.self) { (provider, error) in
+                    if let url = provider as? URL {
+                        self.fetchImage(url: url.imageURL, placeholderContext: placeholderContext)
+                    }
+                }
+            }
+        }
+    }
+    
+    private func fetchImage(url: URL, placeholderContext: UICollectionViewDropPlaceholderContext) {
+        DispatchQueue.global(qos: .userInitiated).async { [weak self] in
+            let urlContents = try? Data(contentsOf: url)
+            DispatchQueue.main.async {
+                if let imageData = urlContents {
+                    placeholderContext.commitInsertion(dataSourceUpdates: {insertionIndexPath in
+                        self?.images.insert(UIImage(data: imageData)!, at: insertionIndexPath.item)
+                    })
+                } else {
+                    placeholderContext.deletePlaceholder()
+                }
+            }
+        }
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, dropSessionDidUpdate session: UIDropSession, withDestinationIndexPath destinationIndexPath: IndexPath?) -> UICollectionViewDropProposal {
+        let isSelf = (session.localDragSession?.localContext as? UICollectionView) == collectionView
+        return UICollectionViewDropProposal(operation: isSelf ? .move : .copy, intent: .insertAtDestinationIndexPath)
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, itemsForBeginning session: UIDragSession, at indexPath: IndexPath) -> [UIDragItem] {
+        session.localContext = collectionView
+        return dragItems(at: indexPath)
+    }
+    
+    private func dragItems(at indexPath: IndexPath) -> [UIDragItem] {
+        if let image = (imageGalleryView.cellForItem(at: indexPath) as? ImageGalleryCollectionViewCell)?.imageView.image {
+            let dragItem = UIDragItem(itemProvider: NSItemProvider(object: image))
+            dragItem.localObject = image
+            return [dragItem]
+        } else {
+            return []
+        }
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, canHandle session: UIDropSession) -> Bool {
+        let isSelf = (session.localDragSession?.localContext as? UICollectionView) == collectionView
+        if isSelf {
+            return session.canLoadObjects(ofClass: UIImage.self)
+        } else {
+            return session.canLoadObjects(ofClass: UIImage.self) && session.canLoadObjects(ofClass: NSURL.self)
+        }
+    }
 }
 
